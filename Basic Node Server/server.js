b232bed623aedef24307541f44fc0709ded59388
Tushar <tushar.rawat030@gmail.com>
2015-09-21 14:31:42 -0500

    Basic Node Server Creation

--- Basic Node Server/server.js
+++ Basic Node Server/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 function onRequest(request, response) {
 	console.log("a user made a request" + request.url);
 	response.writeHead(200, {"Context-Type": "text/plain"});
-	response.write("This is a Demo Response");
+	response.write("This is a demo response");
 	response.end();
 }
 
